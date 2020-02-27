9d0cff99a388b568c849cf9cb7eb16d0201ba75d
McDusia <magdalno222@gmail.com>
2020-02-27 10:54:06 +0100

    non working linear regression

--- regression/linear_regression.py
+++ regression/linear_regression.py
@@ -4,14 +4,14 @@ import matplotlib.pyplot as plt
 
 
 class LinearRegression:
-    def __init__(self, X, y, learning_rate=0.01, n_iters=1500):
+    def __init__(self, _x, _y, learning_rate=0.01, n_iters=1500):
         self.learning_rate = learning_rate
         self.n_iters = n_iters
-        self.n_samples = len(y)
-        self.n_features = np.size(X, 1)
-        self.X = normalization(X, len(y))
-        self.y = y[:, np.newaxis]
-        self.params = np.zeros((self.n_features + 1, 1))
+        self.n_samples = len(_y)
+        self.n_features = np.size(_x, 1)
+        self.X = normalization(_x, len(_y))
+        self.y = _y[:, np.newaxis]
+        self.params = np.zeros((np.size(_x, 1), 1))
         self.J_history = np.zeros((n_iters, 1))
         self.coef_ = None
         self.intercept_ = None
@@ -53,15 +53,15 @@ class LinearRegression:
 
 # (1/n_samples) * X.T @ (X @ params - y) - pochodna cząstkowa funkcji kosztu
 # params holds the updated parameter values according to the update rule.
-#def gradient_descent(X, y, params, learning_rate, n_iters, n_samples, J_history):
-    #n_samples = len(y)
-    #J_history = np.zeros((n_iters, 1))
+def gradient_descent(X, y, params, learning_rate, n_iters):
+    n_samples = len(y)
+    J_history = np.zeros((n_iters, 1))
 
-#    for i in range(n_iters):
- #       params = params - (learning_rate/n_samples) * X.T @ (X @ params - y)
-  #      J_history[i] = compute_cost(X, y, params)
+    for i in range(n_iters):
+        params = params - (learning_rate/n_samples) * X.T @ (X @ params - y)
+        J_history[i] = compute_cost(X, y, params)
 
-   # return J_history, params
+    return J_history, params
 
 # normalization, rescaling the values into a range of [0,1]
     # to boost our accuracy while lowering the cost (error).
@@ -86,28 +86,30 @@ y = dataset.target[:, np.newaxis]    # target values, labels,
 # np.newaxis function - increase the dimension of an array by one more dimension
 
 print("Total samples in our dataset is: {}".format(X.shape[0]))
-#n_samples = len(y)
+n_samples = len(y)
 
 #mu = np.mean(X, 0)
 #sigma = np.std(X, 0)
 #X = (X - mu) / sigma
 #X = np.hstack((np.ones((n_samples, 1)), X))
-#X = normalization(X, n_samples)
+
+X = normalization(X, n_samples)
 n_features = np.size(X, 1)
 params = np.zeros((n_features, 1))
 
-lin = LinearRegression(X, y, 0.01, 1500)
+#lin = LinearRegression(X, y, 0.01, 1500)
 
 initial_cost = compute_cost(X, y, params)
 print("Initial cost is: ", initial_cost, "\n")
 
-#(J_history, optimal_params) = gradient_descent(X, y, params, 0.01, 1500)
+(J_history, optimal_params) = gradient_descent(X, y, params, 0.01, 1500)
 
-lin.fit()
+#lin.fit()
 
-#print("Optimal parameters are: \n", lin.params, "\n")
-print("Final cost is: ", lin.J_history[-1])
-plt.plot(range(len(lin.J_history)), lin.J_history, 'r')
+print("Optimal parameters are: \n", params, "\n")
+
+print("Final cost is: ", J_history[-1])
+plt.plot(range(len(J_history)), J_history, 'r')
 
 plt.title("Convergence Graph of Cost Function")
 plt.xlabel("Number of Iterations")
