9fd6e4e2ddf15b42a7b117558602ee3d9041eadc
McDusia <magdalno222@gmail.com>
2020-05-14 21:36:40 +0200

    cleanups

--- utils/compare_implementations.py
+++ utils/compare_implementations.py
@@ -1,8 +1,6 @@
 import logging
-import pandas as pd
 from configuration.configuration_constants import excluded_values, \
-    target_column_name, \
-    limit_date
+    target_column_name, limit_date
 from utils.DataSplitter import DataSplitter
 from utils.database_handler import DatabaseHandler
 from regression.sklearn_regression import run_sklearn_linear_regression
@@ -20,7 +18,7 @@ def compare_implementations():
     logging.debug("Total samples in our dataset is: {}".format(data.shape[0]))
 
     [x_train_set, x_test_set, y_train_set, y_test_set] \
-        = DataSplitter(data, target_column_name=target_column_name).get_x_y_data()
+        = DataSplitter(data, target_column_name=target_column_name).get_x_y_data_train_test()
     # [sklearn_train_accuracy, sklearn_test_accuracy] = run_sklearn_linear_regression(x_train_set, x_test_set,
     #                                                                                 y_train_set, y_test_set)
     [svm_train_accuracy, svm_test_accuracy] = run_svm_regression(x_train_set, x_test_set,
