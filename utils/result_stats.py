19a280f0f7248348c7d25cadc438d616f37c6d93
McDusia <magdalno222@gmail.com>
2020-06-13 17:24:02 +0200

    cleanups

--- utils/result_stats.py
+++ utils/result_stats.py
@@ -6,17 +6,8 @@ from utils.plots.basic_plot import values_one_plot_with_linear_and_dots, values_
 
 
 def mean_absolute_percentage_error(y_true, y_pred):
-    # y_true = check_array(y_true)
-    # y_pred = check_array(y_pred)
-
-    # Note: does not handle mix 1d representation
-    # if _is_1d(y_true):
-    #    y_true, y_pred = _check_1d_array(y_true, y_pred)
-    y_true, y_pred = y_true + 1, y_pred + 1#np.array(y_true), np.array(y_pred)
-
+    y_true, y_pred = y_true + 1, y_pred + 1
     return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
-
-    #return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
 
 
 def rmse(predictions, targets):
@@ -24,23 +15,15 @@ def rmse(predictions, targets):
 
 
 def get_result_statistics(predicted_values, real_values, plot_title=""):
+    max_error_val = max_error(real_values, predicted_values)
+    r2_score_result = r2_score(real_values, predicted_values)
+    explained_variance_score_result = explained_variance_score(real_values, predicted_values)
     mse = mean_squared_error(real_values, predicted_values)
     rms = rmse(predicted_values, real_values)
-    mean_absolute_percentage_e = mean_absolute_percentage_error(real_values, predicted_values)
     mae = mean_absolute_error(real_values, predicted_values)
-
-    max_error_val = max_error(real_values, predicted_values)
+    mean_absolute_percentage_e = mean_absolute_percentage_error(real_values, predicted_values)
 
     logging.info('Max error: %.3f', round(max_error_val, 6))
-    # mean_absolute_percentage_error = np.mean(np.abs(predicted_values - real_values) / real_values) * 100
-    # Calculate and log accuracy
-    # accuracy = 100 - np.mean(mean_absolute_percentage_error)
-    # logging.info('Accuracy: %s percentage', round(accuracy, 6))
-
-    # Best result: 1.0 [explained_variance_score_result, r2_score_result]
-
-    explained_variance_score_result = explained_variance_score(real_values, predicted_values)
-    r2_score_result = r2_score(real_values, predicted_values)
     logging.info('r2 score: %.3f', round(r2_score_result, 6))
     logging.info('Explained variance score: %0.3f', round(explained_variance_score_result, 6))
     logging.info("Mean squared error: %.3f", mse)
